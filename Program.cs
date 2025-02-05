6c631b0b8f0d5b8fa32477f2ae2ed4fac38d54e5
ZakaDev22 <elfakharzakaria22@gmail.com>
2025-02-05 23:54:32 +0000

    Add New Student Using EF Delete Triggers From Student Tabels To Let The Add Operation Success Without Any Exceptions

--- Program.cs
+++ Program.cs
@@ -4,21 +4,39 @@
 
 using EF_Core_Practices;
 
+// Print All
+//using (var context = new AppDBContext())
+//{
+//    Console.WriteLine($"\n\nPrint All The Records In Students Tabel Using EF Core");
+//    foreach (var Student in context.students)
+//    {
+//        Console.WriteLine(Student);
+//    }
+//}
+
+//// Print By ID
+//int IdToRetrive = 1;
+//using (var context = new AppDBContext())
+//{
+//    Console.WriteLine($"\n\nPrint Record With ID {IdToRetrive} ");
+
+//     var student = context.students.FirstOrDefault(x => x.StudentID == IdToRetrive);
+
+//     Console.WriteLine(student);
+//}
+
+// Add Student
+
+var student = new Students
+{
+    Name = "Zakaria",
+    Subject = "Programming",
+    Grade = 80,
+    IsActive = true
+};
+
 using (var context = new AppDBContext())
 {
-    Console.WriteLine($"\n\nPrint All The Records In Students Tabel Using EF Core");
-    foreach (var Student in context.students)
-    {
-        Console.WriteLine(Student);
-    }
-}
-
-int IdToRetrive = 1;
-using (var context = new AppDBContext())
-{
-    Console.WriteLine($"\n\nPrint Record With ID {IdToRetrive} ");
-
-    var student = context.students.FirstOrDefault(x => x.StudentID == IdToRetrive);
-
-    Console.WriteLine(student);
+    context.students.Add(student);
+    context.SaveChanges();
 }
