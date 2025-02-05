2434e50505cf456cef7a7af2bfda7eaed3836c53
ZakaDev22 <elfakharzakaria22@gmail.com>
2025-02-05 21:27:05 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+
+
+
+using Microsoft.Extensions.Configuration;
+
+var config = new ConfigurationBuilder()
+           .AddJsonFile("appSettings.json")
+           .Build();
+var ConnectionString = config.GetSection("ConnectionStrings").Value;
+
+Console.WriteLine(ConnectionString);
