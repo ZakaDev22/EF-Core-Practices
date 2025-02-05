fcb8422f34c5d173126c12dbded064184a1fe74d
ZakaDev22 <elfakharzakaria22@gmail.com>
2025-02-05 23:35:07 +0000

    Retrive Student By ID Result : Success

--- Program.cs
+++ Program.cs
@@ -6,8 +6,19 @@ using EF_Core_Practices;
 
 using (var context = new AppDBContext())
 {
+    Console.WriteLine($"\n\nPrint All The Records In Students Tabel Using EF Core");
     foreach (var Student in context.students)
     {
         Console.WriteLine(Student);
     }
+}
+
+int IdToRetrive = 1;
+using (var context = new AppDBContext())
+{
+    Console.WriteLine($"\n\nPrint Record With ID {IdToRetrive} ");
+
+    var student = context.students.FirstOrDefault(x => x.StudentID == IdToRetrive);
+
+    Console.WriteLine(student);
 }
