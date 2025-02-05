22959f67d3fc044c644df9cd0b59699b757067d0
ZakaDev22 <elfakharzakaria22@gmail.com>
2025-02-05 22:57:16 +0000

    Add My App DB Context Class With The its Configs

---
+++ AppDBContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+
+namespace EF_Core_Practices
+{
+    public class AppDBContext : DbContext
+    {
+        public DbSet<Students> students { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+
+            var config = new ConfigurationBuilder()
+                       .AddJsonFile("appSettings.json")
+                       .Build();
+            var ConnectionString = config.GetSection("ConnectionStrings").Value;
+
+            optionsBuilder.UseSqlServer(ConnectionString);
+        }
+    }
+}
