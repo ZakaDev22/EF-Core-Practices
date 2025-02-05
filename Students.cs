2066e56014727349e75c3f5835164cb8f1645cf8
ZakaDev22 <elfakharzakaria22@gmail.com>
2025-02-05 22:20:46 +0000

    Add Student  Class Update The Connection String To Match The Students Data Base

---
+++ Students.cs
@@ -0,0 +1,11 @@
+﻿namespace EF_Core_Practices
+{
+    public class Students
+    {
+        public int StudentID { get; set; }
+        public string Name { get; set; }
+        public string Subject { get; set; }
+        public int Grade { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
